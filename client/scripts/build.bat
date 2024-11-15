0f3065b47dcc30970a33840cb95b6cc869927fb6
fodevac <fodevac@gmail.com>
2024-11-15 09:10:42 -0500

    Compose

--- client/scripts/build.bat
+++
@@ -1,6 +0,0 @@
-
-copy node_modules\@ricky0123\vad-web\dist\silero_vad.onnx dist
-copy node_modules\@ricky0123\vad-web\dist\vad.worklet.bundle.min.js dist
-copy node_modules\onnxruntime-web\dist\ort-wasm-simd-threaded.mjs dist
-
-xcopy node_modules\onnxruntime-web\dist\*.wasm dist\ /s /e /y
