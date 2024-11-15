c4981b67b2b02fe0cf3b48a89728fd75fd1c7d13
fodevac <fodevac@gmail.com>
2024-11-15 11:31:02 -0500

    removed db file from version control

--- server/db/reset.sh
+++
@@ -1,8 +0,0 @@
-rm hackaton.db
-
-echo "Applying migrations..."
-
-sqlite3 hackaton.db < migrations/001_create_tables.sql
-sqlite3 hackaton.db < migrations/002_insert_sample_data.sql
-
-echo "Database has been reset."
