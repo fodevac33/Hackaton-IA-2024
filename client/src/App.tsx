6a57fe70c930b21ee673ac93d0d8c284553d3fcf
JCardenas05 <jocardenash@unal.edu.co>
2024-11-15 08:24:54 -0500

    stt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,43 @@
-import { useMicVAD, utils } from "@ricky0123/vad-react";
+import { useState } from "react";
+import { useMicVAD } from "@ricky0123/vad-react";
 import { ort } from "@ricky0123/vad-web/dist/real-time-vad";
+import { transcribeAudio } from "./services/transcriptionService";
 
 // Configuración del entorno ONNX Runtime
 ort.env.wasm.wasmPaths = "https://unpkg.com/onnxruntime-web@dev/dist/";
 
 function App() {
+  const [micActive, setMicActive] = useState(true);
+
   const vad = useMicVAD({
     startOnLoad: true,
     onSpeechEnd: async (audio) => {
       try {
-        const wavBuffer = utils.encodeWAV(audio);
-        const blob = new Blob([wavBuffer], { type: "audio/wav" });
-
-        const formData = new FormData();
-        formData.append("file", blob, "audio.wav"); 
-        formData.append("model", "whisper-large-v3-turbo"); 
-        formData.append("temperature", "0"); 
-        formData.append("response_format", "json"); 
-        formData.append("language", "es"); 
-
-        // const apiKey = process.env.REACT_APP_GROQ_API_KEY;
-        const response = await fetch("https://api.groq.com/openai/v1/audio/transcriptions", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer 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`, // Clave API desde variables de entorno
-          },
-          body: formData,
-        });
-
-        if (response.ok) {
-          const result = await response.json();
-          console.log("Transcripción recibida:", result);
-        } else {
-          console.error("Error en la transcripción:", response.statusText);
-        }
+        const result = await transcribeAudio(audio);
+        console.log("Transcripción recibida:", result);
       } catch (error) {
         console.error("Error al procesar el audio:", error);
       }
     },
   });
 
+  const handleButtonClick = () => {
+    if (micActive) {
+      vad.start(); 
+      console.log("Micrófono activado", micActive)
+    } else {
+      vad.pause(); 
+      console.log
+    }
+    setMicActive(!micActive);
+  };
+
   return (
     <div>
-      {vad.userSpeaking ? "User is speaking" : "No speech detected"}
+      <button onClick={handleButtonClick}>
+        {micActive ? "Desactivar Micrófono" : "Activar Micrófono"}
+      </button>
+      <p>{vad.userSpeaking ? "El usuario está hablando..." : "No se detectó habla"}</p>
     </div>
   );
 }
